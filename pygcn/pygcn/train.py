44529a5bd1fe14d890610446c72cfbb0a0bf62a6
Vinay Sawal <vinaysawal@gmail.com>
2019-11-26 19:14:17 -0800

    Commenting out print statuements

--- pygcn/pygcn/train.py
+++ pygcn/pygcn/train.py
@@ -273,7 +273,7 @@ def test():
     with torch.no_grad():
         output = model(features, adj)
         loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-        acc_test = accuracy_new(output[idx_test], labels[idx_test])
+        acc_test = accuracy(output[idx_test], labels[idx_test])
         print("Test set results:",
             "loss= {:.4f}".format(loss_test.item()),
             "accuracy= {:.4f}".format(acc_test.item()))
