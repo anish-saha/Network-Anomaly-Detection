c0ac2012309bfa4b0320f71742034b50eed012b2
Anish <asaha@berkeley.edu>
2019-11-27 23:09:06 -0800

    Hyperparmeter tuning pipeline completed

--- avd/learners/sklearner.py
+++ avd/learners/sklearner.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn import linear_model, svm, tree, ensemble, feature_extraction, preprocessing
 from sklearn.metrics import recall_score, precision_score, accuracy_score, roc_auc_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import IsolationForest
 
 from avd.configs.config import *
@@ -23,8 +24,9 @@ def encode_labels(labels):
 
 
 class SkLearner(AbstractLearner):
-    def __init__(self, classifier=None, labels=None):
+    def __init__(self, classifier=None, labels=None, params=None):
         super(SkLearner, self).__init__(classifier)
+        self._params = params
         if labels is not None and isinstance(labels, dict):
             if len(labels) == 2:
                 self.fit_labels([labels['neg'], labels['pos']])
@@ -60,35 +62,100 @@ class SkLearner(AbstractLearner):
         return DataSetFactory().convert_data_to_sklearn_format(features, labels, feature_id_col_name, metadata_cols)
 
     def set_decision_tree_classifier(self, tree_number=100):
-        return SkLearner(tree.DecisionTreeClassifier())
+        params = {'bootstrap': [True, False],
+                  'max_depth': [10, 20, 40, 60, 80, 100, None],
+                  'max_features': ['auto', 'sqrt'],
+                  'min_samples_leaf': [1, 2, 4],
+                  'min_samples_split': [2, 5, 10],
+                  'n_estimators': [100, 200, 400, 600, 800, 1000]}
+        return SkLearner(tree.DecisionTreeClassifier(random_state=42), params=params)
 
     def set_svm_classifier(self):
-        return SkLearner(svm.SVC(), self._label_encoder)
+        params = {'C': [0.001, 0.01, 0.1, 1, 10],
+                  'gammas': [0.001, 0.01, 0.1, 1],
+                  'tol': [0.0001, 0.0005, 0.001, 0.005, 0.01],
+                  'kernel': ['rbf', 'linear']}
+        return SkLearner(svm.SVC(random_state=42), labels=self._label_encoder, params=params)
 
     def set_randomforest_classifier(self):
-        return SkLearner(ensemble.RandomForestClassifier(n_estimators=100, criterion="entropy"))
+        params = {'bootstrap': [True, False],
+                  'max_depth': [10, 20, 40, 60, 80, 100, None],
+                  'max_features': ['auto', 'sqrt'],
+                  'min_samples_leaf': [1, 2, 4],
+                  'min_samples_split': [2, 5, 10],
+                  'n_estimators': [100, 200, 400, 600, 800, 1000]}
+        return SkLearner(ensemble.RandomForestClassifier(random_state=42, n_jobs=-1, criterion="entropy"), params=params)
 
     def set_adaboost_classifier(self):
-        return SkLearner(ensemble.AdaBoostClassifier())
+        params = {'n_estimators': [50, 100, 200, 400, 600, 800, 1000],
+                  'learning_rate' : [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1],
+                  'algorithm': ['SAMME', 'SAMME.R']}
+        return SkLearner(ensemble.AdaBoostClassifier(random_state=42), params=params)
 
     def set_rf_bagging_classifier(self):
+        params = {'base_estimator__max_depth' : [10, 20, 40, 60, 80, 100, None],
+                  'base_estimator__n_estimators' : [100, 200, 400, 600, 800, 1000],
+                  'n_estimators': [50, 100, 200, 400, 600, 800, 1000],
+                  'max_samples' : [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1],
+                  'bootstrap': [True, False],
+                  'oob_score': [True, False]}
         return SkLearner(ensemble.BaggingClassifier(
-            ensemble.RandomForestClassifier(n_estimators=100, criterion="entropy")))
+            ensemble.RandomForestClassifier(criterion="entropy"), random_state=42, n_jobs=-1), params=params)
 
     def set_bagging_classifier(self):
-        return SkLearner(ensemble.BaggingClassifier(tree.DecisionTreeClassifier()))
+        params = {'base_estimator__max_depth' : [10, 20, 40, 60, 80, 100, None],
+                  'base_estimator__n_estimators' : [100, 200, 400, 600, 800, 1000],
+                  'n_estimators': [50, 100, 200, 400, 600, 800, 1000],
+                  'max_samples' : [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1],
+                  'bootstrap': [True, False],
+                  'oob_score': [True, False]}
+        return SkLearner(ensemble.BaggingClassifier(tree.DecisionTreeClassifier(), random_state=42, n_jobs=-1), params=params)
 
     def set_gradient_boosting_classifier(self):
-        return SkLearner(ensemble.GradientBoostingClassifier(loss='exponential', n_estimators=500, subsample=0.9))
+        params = {'min_samples_leaf': [1, 2, 4],
+                  'min_samples_split': [2, 5, 10],
+                  'max_features': ['auto', 'sqrt'],
+                  'subsample':[0.7, 0.8, 0.9, 1],
+                  'n_estimators': [50, 100, 200, 400, 600, 800, 1000],
+                  'max_depth': [10, 20, 40, 60, 80, 100, None]}
+        return SkLearner(ensemble.GradientBoostingClassifier(loss='exponential', random_state=42), params=params)
 
     def set_logistic_regression_classifier(self):
-        return SkLearner(linear_model.LogisticRegression(random_state=0, solver='lbfgs', max_iter=200, multi_class='ovr'))
+        params = {'dual': [True, False],
+                  'max_iter': [75, 100, 125, 150],
+                  'C': [0.5, 1.0, 1.5, 2.0, 2.5],
+                  'penalty': ['l1', 'l2'],
+                  'tol': [0.00001, 0.00005, 0.0001, 0.0005, 0.001]}
+        return SkLearner(linear_model.LogisticRegression(random_state=42, solver='lbfgs', multi_class='ovr'), params=params)
 
     def set_isolation_forest_classifier(self):
+        '''
+        Deprecated for now, no meaningful results - performance metrics were similar to baseline results.
+        '''
         return SkLearner(ensemble.IsolationForest(max_samples=100, random_state=42, contamination=0.1))
 
+    def optimize_hyperparameters(self):
+        '''
+        Performs hyperparameter optimization using the RandomizedSearchCV library.
+        Random search will have a 99% chance probability of finding a combination of hyperparameters 
+        within the 5% optima with 90 iterations; also avoids issues related to finding local optima.
+        Formula: $\P(X_{opt}) = 1 - (1-0.05)^n$, for $n$ independently sampled random search iterations
+        '''
+        if self._params:
+            # print(self._classifier.get_params().keys())
+            return RandomizedSearchCV(estimator=self._classifier, param_distributions=self._params, 
+                                      n_iter=90, cv=2, verbose=2, random_state=42, n_jobs=-1)
+        else:
+            print("No hyperparameters specified. Using default hyperparameters for classifier.")
+            return self._classifier
+    
     def train_classifier(self, dataset):
-        self._classifier = self._classifier.fit(dataset.features, dataset.labels)
+        # RandomizedSearchCV to optimize parameters
+        self._classifier = self.optimize_hyperparameters()
+        # Train classifier on dataset
+        print("Training classifier...")
+        self._classifier = self._classifier.fit(dataset.features, dataset.labels).best_estimator_
+        print("Optimized hyperparameters:\n" + self._classifier.get_params()
         return self
 
     def get_prediction(self, prediction_data):
@@ -116,14 +183,12 @@ class SkLearner(AbstractLearner):
         true_negative = float(
             len(np.where(l_test + prediction == 0)[0]))  # 0 (truth) - 0 (prediction) == 0 which is a true positive
 
-        metrics = {
-                "auc": roc_auc_score(l_test, probas),
-                "recall": recall_score(l_test, prediction),
-                "precision": precision_score(l_test, prediction),
-                "accuracy": accuracy_score(l_test, prediction),
-                "fpr": false_positive / (true_negative + false_positive),
-                "tnr": true_negative / (true_negative + false_positive)
-                }
+        metrics = {"auc": roc_auc_score(l_test, probas),
+                   "recall": recall_score(l_test, prediction),
+                   "precision": precision_score(l_test, prediction),
+                   "accuracy": accuracy_score(l_test, prediction),
+                   "fpr": false_positive / (true_negative + false_positive),
+                   "tnr": true_negative / (true_negative + false_positive)}
         return metrics
 
     def cross_validate(self, dataset, n_folds=10):
@@ -131,9 +196,9 @@ class SkLearner(AbstractLearner):
         for train_index, test_index in self.split_kfold(dataset.features, dataset.labels, n_folds):
             f_train, f_test = dataset.features[train_index], dataset.features[test_index]
             l_train, l_test = dataset.labels[train_index], dataset.labels[test_index]
-            # prediction = self.train_classifier(DataSet(f_train, l_train)).predict(f_test)
-            prediction = self.train_classifier(DataSet(f_train, l_train)).get_prediction(f_test)
-            probas = self.train_classifier(DataSet(f_train, l_train)).get_prediction_probabilities(f_test)[:, 1]
+            model = self.train_classifier(DataSet(f_train, l_train))
+            prediction = model.get_prediction(f_test)
+            probas = model.get_prediction_probabilities(f_test)[:, 1]
             metrics = self.get_classification_metrics(l_test, prediction, probas)
             roc_auc.append(metrics["auc"])
             recall.append(metrics["recall"])  # TPR
