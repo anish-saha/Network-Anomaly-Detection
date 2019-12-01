8727471cd87955a8ca164e47a5fc0d1ce0eff969
Vinay Sawal <vinaysawal@gmail.com>
2019-12-01 09:44:00 -0800

    added confusion matrix in evaluation

--- pygcn/train.py
+++ pygcn/train.py
@@ -5,6 +5,7 @@ import time
 import argparse
 import numpy as np
 import scipy.sparse as sp
+from sklearn.metrics import confusion_matrix
 
 import torch
 import torch.nn.functional as F
@@ -188,7 +189,9 @@ def accuracy(output, labels):
     preds = output.max(1)[1].type_as(labels)
     correct = preds.eq(labels).double()
     correct = correct.sum()
-    return correct / len(labels)
+    acc = correct / len(labels)
+    confusion_m = confusion_matrix(labels, preds).ravel()
+    return (acc, confusion_m)
 
 def accuracy_new(output, labels):
     print("output:", output)
@@ -245,12 +248,15 @@ def train(epoch):
     t = time.time()
     model.train()
     output = model(features, adj)
-    #print("output:", output)
     loss_train = F.nll_loss(output[idx_train], labels[idx_train])
-    acc_train = accuracy(output[idx_train], labels[idx_train])
+    acc_train, confusion_matrix = accuracy(output[idx_train], labels[idx_train])
     optimizer.zero_grad()
     loss_train.backward()
     optimizer.step()
+
+    print('###### Epoch: {:04d} ######'.format(epoch+1))
+    print('Training: Loss={:04f} Accuracy={:04f}'.format(loss_train.item(),acc_train.item()))
+    print('Training: Confusion Matrix(tn,fp,fn,tp)=', confusion_matrix)
 
     if not args.fastmode:
         # Evaluate validation set performance separately,
@@ -259,24 +265,20 @@ def train(epoch):
         output = model(features, adj)
 
     loss_val = F.nll_loss(output[idx_val], labels[idx_val])
-    acc_val = accuracy(output[idx_val], labels[idx_val])
-    print('Epoch: {:04d}'.format(epoch+1),
-          'loss_train: {:.4f}'.format(loss_train.item()),
-          'acc_train: {:.4f}'.format(acc_train.item()),
-          'loss_val: {:.4f}'.format(loss_val.item()),
-          'acc_val: {:.4f}'.format(acc_val.item()),
-          'time: {:.4f}s'.format(time.time() - t))
-
+    acc_val, confusion_matrix = accuracy(output[idx_val], labels[idx_val])
+    print('Validation: Loss={:04f} Accuracy={:04f}'.format(loss_val.item(),acc_val.item()))
+    print('Validation: Confusion Matrix(tn,fp,fn,tp)=', confusion_matrix)
 
 def test():
     model.eval()
     with torch.no_grad():
         output = model(features, adj)
         loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-        acc_test = accuracy(output[idx_test], labels[idx_test])
-        print("Test set results:",
-            "loss= {:.4f}".format(loss_test.item()),
-            "accuracy= {:.4f}".format(acc_test.item()))
+        acc_test, confusion_matrix = accuracy(output[idx_test], labels[idx_test])
+        print("==========Test set results==========")
+        print("Loss= {:.4f}".format(loss_test.item()),
+            "Accuracy= {:.4f}".format(acc_test.item()))
+        print('Confusion Matrix (tn, fp, fn, tp)=', confusion_matrix)
 
 
 
