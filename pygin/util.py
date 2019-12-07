c9457eb72793227624f9fefeae75e173c754e328
Vincent Ying <vincenthying@gmail.com>
2019-12-06 22:56:03 -0800

    Melding pygin's load_data()

--- pygin/util.py
+++ pygin/util.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+import scipy.sparse as sp
 import random
 import torch
 from sklearn.model_selection import StratifiedKFold
@@ -23,18 +24,30 @@ class S2VGraph(object):
 
         self.max_neighbor = 0
 
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
 def load_data(dataset, degree_as_tag):
     """Load Twitter dataset"""
     print('Loading {} dataset...'.format(dataset))
 
     path="../data/" + dataset + "/"
-    idx_features_labels = np.genfromtxt("{}{}.features_mid".format(path, dataset),
+    idx_features_labels = np.genfromtxt("{}{}.features_small".format(path, dataset),
                                         delimiter=',',
                                         dtype=np.dtype(str))
 
     # delete rows and columns that are not required
     idx_features_labels = np.delete(idx_features_labels, np.s_[0], axis=0)
+    print("idx_features_labels:", idx_features_labels)
     idx_features_labels = np.delete(idx_features_labels, np.s_[1:3], axis=1)
+    print("after removing columns idx_features_labels:", idx_features_labels)
 
     # shuffle the rows
     np.random.shuffle(idx_features_labels)
@@ -45,15 +58,19 @@ def load_data(dataset, degree_as_tag):
     # build graph
     idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
     idx_map = {j: i for i, j in enumerate(idx)}
-    edges_unordered = np.genfromtxt("{}{}.graph_mid".format(path, dataset),
+    print(idx_map)
+    edges_unordered = np.genfromtxt("{}{}.graph_small".format(path, dataset),
                                     delimiter=',',
                                     dtype=np.int32)
 
     # delete rows that we don't need
     edges_unordered = np.delete(edges_unordered, np.s_[0], axis=0)
+    print(edges_unordered)
 
     x = list(map(idx_map.get, edges_unordered.flatten()))
+    print("x:", x)
     y = [0 if i is None else i for i in x]
+    print("y:", y)
     edges = np.array(y, dtype=np.int32)
     edges = edges.reshape(edges_unordered.shape)
     print("edges shape:", edges.shape)
@@ -66,28 +83,109 @@ def load_data(dataset, degree_as_tag):
     adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
 
     features = normalize_features(features)
+    print("features:", features)
     adj = normalize_adj(adj + sp.eye(adj.shape[0]))
+    print("adj:", adj)
 
     features = torch.FloatTensor(np.array(features.todense()))
     labels = torch.LongTensor(np.where(labels)[1])
     adj = torch.FloatTensor(np.array(adj.todense()))
 
-    idx_train = list(range(0, 3000))
-    idx_val = list(range(3000, 3900))
-    idx_test = list(range(4000, 4900))
+    g_list = []
+    label_dict = {}
+    feat_dict = {}
+    for row in idx_features_labels:
+        n, l = [int(w) for w in row]
+        l = row[1] 
+        if not l in label_dict:
+            mapped = len(label_dict)
+            label_dict[l] = mapped
+        g = nx.Graph()
+        node_tags = []
+        node_features = []
+        n_edges = 0
+        # Each Node
+        for j in range(n):
+            g.add_node(j)
+            row = f.readline().strip().split()
+            tmp = int(row[1]) + 2
 
-    np.random.shuffle(idx_train)
-    np.random.shuffle(idx_val)
-    np.random.shuffle(idx_test)
+            if tmp == len(row):
+                # no node attributes
+                row = [int(w) for w in row]
+                attr = None
+            else:
+                row = [int(w) for w in row[:tmp]]
+                attr = np.array([float(w) for w in row[tmp:]])
 
-    idx_train = torch.LongTensor(idx_train)
-    idx_val = torch.LongTensor(idx_val)
-    idx_test = torch.LongTensor(idx_test)
+            feat = row[0]
+            if not feat in feat_dict:
+                mapped = len(feat_dict)
+                feat_dict[feat] = mapped
+            node_tags.append(feat_dict[feat])
 
-    return adj, features, labels, idx_train, idx_val, idx_test
+            if tmp > len(row):
+                node_features.append(attr)
+
+            n_edges += row[1]
+            # Each Edge
+            for k in range(2, len(row)):
+                g.add_edge(j, row[k])
+
+        if node_features != []:
+            node_features = np.stack(node_features)
+            node_feature_flag = True
+        else:
+            node_features = None
+            node_feature_flag = False
+
+        g_list.append(S2VGraph(g, l, node_tags))
+
+    #add labels and edge_mat       
+    for g in g_list:
+        g.neighbors = [[] for i in range(len(g.g))]
+        for i, j in g.g.edges():
+            g.neighbors[i].append(j)
+            g.neighbors[j].append(i)
+        degree_list = []
+        for i in range(len(g.g)):
+            g.neighbors[i] = g.neighbors[i]
+            degree_list.append(len(g.neighbors[i]))
+        g.max_neighbor = max(degree_list)
+
+        g.label = label_dict[g.label]
+
+        edges = [list(pair) for pair in g.g.edges()]
+        edges.extend([[i, j] for j, i in edges])
+
+        deg_list = list(dict(g.g.degree(range(len(g.g)))).values())
+        g.edge_mat = torch.LongTensor(edges).transpose(0,1)
+
+    if degree_as_tag:
+        for g in g_list:
+            g.node_tags = list(dict(g.g.degree).values())
+
+    #Extracting unique tag labels   
+    tagset = set([])
+    for g in g_list:
+        tagset = tagset.union(set(g.node_tags))
+
+    tagset = list(tagset)
+    tag2index = {tagset[i]:i for i in range(len(tagset))}
+
+    for g in g_list:
+        g.node_features = torch.zeros(len(g.node_tags), len(tagset))
+        g.node_features[range(len(g.node_tags)), [tag2index[tag] for tag in g.node_tags]] = 1
+
+    print('# classes: %d' % len(label_dict))
+    print('# maximum node tag: %d' % len(tagset))
+    print("# data: %d" % len(g_list))
+
+    #return g_list, len(label_dict)
+    return g_list, int(labels.max()) + 1
 
 
-def load_data(dataset, degree_as_tag):
+def load_data_mutag(dataset, degree_as_tag):
     '''
         dataset: name of dataset
         test_proportion: ratio of test train split
@@ -99,7 +197,7 @@ def load_data(dataset, degree_as_tag):
     label_dict = {}
     feat_dict = {}
 
-    with open('data/%s/%s.txt' % (dataset, dataset), 'r') as f:
+    with open('../data/%s/%s.txt' % (dataset, dataset), 'r') as f:
         n_g = int(f.readline().strip())
         for i in range(n_g):
             row = f.readline().strip().split()
@@ -180,10 +278,8 @@ def load_data(dataset, degree_as_tag):
         g.node_features = torch.zeros(len(g.node_tags), len(tagset))
         g.node_features[range(len(g.node_tags)), [tag2index[tag] for tag in g.node_tags]] = 1
 
-
     print('# classes: %d' % len(label_dict))
     print('# maximum node tag: %d' % len(tagset))
-
     print("# data: %d" % len(g_list))
 
     return g_list, len(label_dict)
@@ -202,5 +298,24 @@ def separate_data(graph_list, seed, fold_idx):
     test_graph_list = [graph_list[i] for i in test_idx]
 
     return train_graph_list, test_graph_list
+
+
+def normalize_adj(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+    r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+    return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
+
+
+def normalize_features(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
 
 
